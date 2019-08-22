93b576cc8172b74d5cabbc5821b0e7fc411790ac
akexinad <danixeka@gmail.com>
2019-08-23 08:24:16 +1000

    methods

---
+++ Section2Classes/Section2Classes/Point.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Section2Classes
+{
+    public class Point
+    {
+        /*
+        * 
+        * OBJECT INITIALIZERS
+        * ===================
+        * 
+        * - Object initializers can be used in order to avoid writing too many constructors.
+        * 
+        * 
+        * 
+        * 
+        * METHODS
+        * =======
+        * 
+        * - Signature Of A Method = Name and the number and type of parameters.
+        * - Overloading Mthods = Having a method with the same name bbut different signatures.
+        * - The Params Modifier = This allows to pass in various object paramters without having to initialize the class that creates the object.
+        * 
+        * The modifiers below are CODE SMELLS
+        * - Ref Modifier
+        * - Out Modifier
+        * 
+        */
+
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Move(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Move(Point newLocation)
+        {
+            //this.X = newLocation.X;
+            //this.Y = newLocation.Y;
+
+            // Instead of repeating the above lines in the overload, we can just call the previous Move method and pass
+            //      in these arguments.
+
+            if (newLocation == null)
+                throw new ArgumentNullException("newLocation");
+                
+            Move(newLocation.X, newLocation.Y);
+        }
+
+    }
+}
--- Section2Classes/Section2Classes/Program.cs
+++ Section2Classes/Section2Classes/Program.cs
@@ -1,11 +1,11 @@
-﻿namespace Section2Classes
+﻿using System;
+
+namespace Section2Classes
 {
     partial class Program
     {
         static void Main(string[] args)
         {
-            
-
             var person = new Person()
             {
                 Name = "Benigni"
@@ -21,6 +21,45 @@
 
             var order = new Order();
             customer.Orders.Add(order);
+
+            Console.Clear();
+
+            //UseOverloadsPoints();
+
+            UseParamsCalculator();
+        }
+
+        static void UseOverloadsPoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(null);
+                Console.WriteLine($"{ point.X }, { point.Y }");
+
+                point.Move(100, 200);
+                Console.WriteLine($"{ point.X }, { point.Y }");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("newLocation cannot be null \n A try catch block will stop your application from running.");
+            }
+
+        }
+
+        static void UseParamsCalculator()
+        {
+            var calculator = new Calculator();
+
+            // With the params keyword in the Add method, you can pass a varying amount of keywords.
+
+            Console.WriteLine(calculator.Add(1, 2, 3));
+            Console.WriteLine(calculator.Add(1, 2));
+            Console.WriteLine(calculator.Add(1, 2, 3, 4, 5, 6));
+
+            // You can also call a new array, but there is no need to.
+            Console.WriteLine(calculator.Add(new int[] { 1, 2, 3 }));
+
         }
     }
 }
