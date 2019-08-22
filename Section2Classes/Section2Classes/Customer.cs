3a308505c7f91b9df0203892bfc8807a321d13cc
akexinad <danixeka@gmail.com>
2019-08-22 19:17:18 +1000

    constructors

---
+++ Section2Classes/Section2Classes/Customer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace Section2Classes
+{
+    public class Customer
+    {
+        /*
+        * CONSTRUCTORS
+        * ============
+        * 
+        * - A constructor is a method that is called when an instance of a class is created.
+        * - It's purpose is to put an object in an early state.
+        * - The constructor have the same name as the class and do not have the return type.
+        * - You can have a paramterless constructor, or .NET will create one for you.
+        * 
+        * 
+        * CONSTRUCTOR OVERLOADING
+        * =======================
+        * 
+        * - You can multiple constructors.
+        * - Constuctor overloading makes initialization of the class easier.
+        * 
+        * 
+        * WHEN TO USE CONSTRUCTORS
+        * ========================
+        * 
+        * - NOTE that you would only use a constructor if you really want to initialize your class with an early state that is essential for the class to perform.
+        * - See the order class below.
+        */
+
+        public int Id;
+        public string Name;
+        public List<Order> Orders;
+
+        public Customer()
+        {
+            // This is an example of the necessity of the constructor
+            Orders = new List<Order>();
+        }
+        public Customer(int id)
+            : this()
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name)
+            : this(id) // THIS LINE HEAR WILL CALL THE OTHER CONSTRUCTORS AND INITIALIZE THEIR VARIABLES.
+        {
+            this.Name = name;
+        }
+
+
+    }
+
+    public class Order
+    {
+
+    }
+}
--- Section2Classes/Section2Classes/Program.cs
+++ Section2Classes/Section2Classes/Program.cs
@@ -16,6 +16,11 @@
             var p = Person.Parse("Pasolini");
             p.Introduce("Luca");
 
+
+            var customer = new Customer();
+
+            var order = new Order();
+            customer.Orders.Add(order);
         }
     }
 }
