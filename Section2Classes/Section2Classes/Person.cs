afd8e8847f3dd3014ec2b1e57378c4382c47ea84
danny <danny.ceccattini@lendlease.com>
2019-08-22 15:55:52 +1000

    clean up

--- Section2Classes/Section2Classes/Person.cs
+++ Section2Classes/Section2Classes/Person.cs
@@ -2,25 +2,54 @@
 
 namespace Section2Classes
 {
-    partial class Program
+
+    public class Person
     {
-        public class Person
+        /*
+         * 
+        * THE BASICS OF A CLASS
+        * =====================
+        * 
+        * - A class is simply a building block of a software application.
+        * 
+        * - Think of a blog. There are 3 layers:
+        *      - Presentation            = The PostView Class
+        *      - Business Logic/Domain   = Post Class
+        *      - Data Access/Persistence = PostRepository Class
+        *      
+        * 
+        * CLASS ANATOMY
+        * 
+        * - Data (represented by fields/attributes)
+        * - Behaviour (repredented by methods/functions)
+        * 
+        * 
+        * - NB: An OBJECT is in essence an instance of a class in memory.
+        * 
+        * 
+        * CLASS MEMBERS
+        * 
+        * - Instance: accessible from an object.
+        * - Static: Accessible from the class.
+        * 
+        * - Static methods are helpful so we do not have to instantiate a class multiple times and have it hogging memory
+        * 
+        */
+
+        public string Name;
+
+        public void Introduce(string to)
         {
-            public string Name;
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
 
-            public void Introduce(string to)
-            {
-                Console.WriteLine("Hi {0}, I am {1}", to, Name);
-            }
+        public static Person Parse(string str)
+        {
+            var person = new Person();
 
-            public static Person Parse(string str)
-            {
-                var person = new Person();
+            person.Name = str;
 
-                person.Name = str;
-
-                return person;
-            }
+            return person;
         }
     }
 }
--- Section2Classes/Section2Classes/Program.cs
+++ Section2Classes/Section2Classes/Program.cs
@@ -4,37 +4,7 @@
     {
         static void Main(string[] args)
         {
-            /*
-             * THE BASICS OF A CLASS
-             * =====================
-             * 
-             * - A class is simply a building block of a software application.
-             * 
-             * - Think of a blog. There are 3 layers:
-             *      - Presentation            = The PostView Class
-             *      - Business Logic/Domain   = Post Class
-             *      - Data Access/Persistence = PostRepository Class
-             *      
-             * 
-             * CLASS ANATOMY
-             * 
-             * - Data (represented by fields/attributes)
-             * - Behaviour (repredented by methods/functions)
-             * 
-             * 
-             * - NB: An OBJECT is in essence an instance of a class in memory.
-             * 
-             * 
-             * CLASS MEMBERS
-             * 
-             * - Instance: accessible from an object.
-             * - Static: Accessible from the class.
-             * 
-             * - Static methods are helpful so we do not have to instantiate a class multiple times and have it hogging memory
-             * 
-             *
-             * 
-             */
+            
 
             var person = new Person()
             {
