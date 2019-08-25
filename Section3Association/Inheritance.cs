d647842184216b2bf67a0cfd2113c722162c6b6b
akexinad <danixeka@gmail.com>
2019-08-26 05:54:03 +1000

    inheritance

---
+++ Section3Association/Inheritance.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Section3Association
+{
+    namespace Inheritance
+        /*
+         * 
+         * INHERITANCE is a kind of relationship between two classes that allows one to inherit code from the other.
+         * 
+         * It is normally referred as an "is a" relationship, such as a car 'is a' vehicle.
+         * 
+         * 
+         */
+    {
+        public class PresentationObject
+        {
+            public int Height { get; set; }
+            public int Width { get; set; }
+
+            public void Copy()
+            {
+                System.Console.WriteLine("Object copied to clipboard.");
+            }
+
+            public void Duplicate()
+            {
+                System.Console.WriteLine("Object was duplicated.");
+            }
+        }
+
+        class Text : PresentationObject
+        {
+            public int FontSize { get; set; }
+            public int FontName { get; set; }
+
+            public void AddHyperlink(string url)
+            {
+                Console.WriteLine($"Add a link to { url }");
+            }
+        }
+    }
+}
---
+++ Section3Association/Program.cs
@@ -0,0 +1,18 @@
+﻿using Section3Association.Inheritance;
+using System;
+
+namespace Section3Association
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = new Text();
+            // Note that the methods that come for free when you create your classes and its derivations are the from the OBJECT CLASS.
+            // All classes automatically inherit from the OBJECT class.
+
+            text.Width = 100;
+            text.Copy();
+        }
+    }    
+}
