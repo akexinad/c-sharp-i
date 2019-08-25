66ef30e4ed6fbdbad0f671e131206a39911aadf9
akexinad <danixeka@gmail.com>
2019-08-26 06:43:28 +1000

    composition

---
+++ Section3Association/Composition.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Section3Association
+{
+    namespace Composition
+    {
+        /*
+         * 
+         * COMPOSITION is a kind of relationship between two classes that allows one to contain the other.
+         * 
+         * - The relationship is described as 'has-a', such as a car 'has-an' engine.
+         * - Composition is far more flexible than inheritance.
+         * 
+         * - A simple example would be the how the Installer and DbMigrator class have a composition relationship with the LOGGER.
+         *      - Both classes HAVE A logging method to provide you with information in the terminal during the installation process.
+         * 
+         */
+
+
+        public class Logger
+        {
+            public void Log(string message)
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+        public class DbMigrator
+        {
+            private readonly Logger _logger;
+
+            public DbMigrator(Logger logger)
+            {
+                _logger = logger;
+            }
+
+            public void Migrate()
+            {
+                _logger.Log("We are migrating");
+            }
+        }
+
+        public class Installer
+        {
+            private readonly Logger _logger;
+
+            public Installer(Logger logger)
+            {
+                _logger = logger;
+            }
+
+            public void Install()
+            {
+                _logger.Log("We are installing a program.");
+            }
+        }
+    }
+}
--- Section3Association/Program.cs
+++ Section3Association/Program.cs
@@ -1,18 +1,43 @@
-﻿using Section3Association.Inheritance;
+﻿using Section3Association.Composition;
+using Section3Association.Inheritance;
 using System;
 
 namespace Section3Association
 {
+
+    /*
+     * 
+     * PROVERB: FAVOUR COMPOSITION OVER INHERITANCE.
+     * 
+     */
+
     class Program
     {
         static void Main(string[] args)
         {
+            // INHERITANCE
+            // ===========
+
             var text = new Text();
             // Note that the methods that come for free when you create your classes and its derivations are the from the OBJECT CLASS.
             // All classes automatically inherit from the OBJECT class.
 
             text.Width = 100;
             text.Copy();
+
+
+
+
+            // COMPOSITION
+            // ===========
+
+            var dbMigrator = new DbMigrator(new Logger());
+
+            var logger = new Logger();
+            var installer = new Installer(logger);
+
+            dbMigrator.Migrate();
+            installer.Install();
         }
-    }    
+    }
 }
