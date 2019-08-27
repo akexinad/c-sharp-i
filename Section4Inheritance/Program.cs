5e4001d89f1b3984b689ba6771581dd54c5dff68
danny <danny.ceccattini@lendlease.com>
2019-08-27 14:23:37 +1000

    upcasting

--- Section4Inheritance/Program.cs
+++ Section4Inheritance/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Section4Inheritance.AccessModifers;
 using Section4Inheritance.Constructors;
+using Section4Inheritance.UpCastingDownCasting;
 
 namespace Section4Inheritance
 {
@@ -10,6 +11,21 @@ namespace Section4Inheritance
             var ferrari = new Car("aks-03t");
 
 
+            // UPCASTING
+            // =========
+
+            var text = new Text();
+            Shape shape = text;
+            // We are going UP the inheritance chain and creating a shape object based off the derived text class.
+            // Both shape and text are pointing to the same object in memory. When you UpCast and DownCast, it provides you
+            //      with different views of that same object.
+
+            text.Width = 200;
+            shape.Width = 100;
+
+            // Remember that text and shape are referencing a point in memory. So if you change the value in one or the other,
+            //      it will change the value in memory.
+            System.Console.WriteLine(text.Width);
         }
     }
 }
---
+++ Section4Inheritance/Shape.cs
@@ -0,0 +1,24 @@
+﻿namespace Section4Inheritance
+{
+    namespace UpCastingDownCasting
+    {
+        public class Shape
+        {
+            public int Width { get; set; }
+            public int Height { get; set; }
+            public int X { get; set; }
+            public int Y { get; set; }
+
+            public void Draw()
+            {
+
+            }
+        }
+
+        public class Text : Shape
+        {
+            public int FontSize { get; set; }
+            public string FontName { get; set; }
+        }
+    }
+}
