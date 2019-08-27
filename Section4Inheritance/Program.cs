cf2c5be15821aa3b7d6e56abbd72fb3642c1d3cf
danny <danny.ceccattini@lendlease.com>
2019-08-28 08:26:05 +1000

    upcasting and downcasting

--- Section4Inheritance/Program.cs
+++ Section4Inheritance/Program.cs
@@ -1,6 +1,10 @@
 ﻿using Section4Inheritance.AccessModifers;
 using Section4Inheritance.Constructors;
 using Section4Inheritance.UpCastingDownCasting;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Section4Inheritance
 {
@@ -25,7 +29,35 @@ namespace Section4Inheritance
 
             // Remember that text and shape are referencing a point in memory. So if you change the value in one or the other,
             //      it will change the value in memory.
-            System.Console.WriteLine(text.Width);
+            Console.WriteLine(text.Width);
+
+            // Since StreamReader inherits from Stream. We can pass any class into this contstructor that derives from the stream class.
+            // Memory stream will AUTOMATICALLY BE UPCASTED to stream.
+            StreamReader reader = new StreamReader(new MemoryStream());
+
+            ArrayList list = new ArrayList();
+            // The list.Add method accepts anything that inherits from the object class.
+            // This means that you can pass anything in here and it will IMPLICITLY CONVERTED TO THE OBJECT CLASS.
+            list.Add(1);
+            list.Add("hello");
+            list.Add(new Text());
+
+            var anotherList = new List<int>();
+
+
+
+            // DOWNCASTING
+            // ===========
+
+            Shape shape2 = new Text();
+            // The above approach of downcasting will only give us a limited view of the object because
+            //      the object is downcasted at compile time not run time.
+
+            // To cast it during compile time, we need to cast it.
+            // Now we have access to the Text object properties.
+            Text text2 = (Text)shape2;
+            
+
         }
     }
 }
