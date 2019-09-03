0f313b2039ad5bbf1d4c174f2b91e1a18c571a4f
danny <danny.ceccattini@lendlease.com>
2019-09-03 16:04:11 +1000

    abstract classes and memebers

---
+++ Section5Polymorphism/Car.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Section5Polymorphism
+{
+    namespace AbstractClasses
+    {
+        public abstract class Automobile
+        {
+            public string Make { get; set; }
+            public string Model { get; set; }
+            public int Year { get; set; }
+
+            // With abstract methods, the engineer is forced to implement all methods within the abstract class.
+            public void Start()
+            {
+                Console.WriteLine("BROOM");
+            }
+
+            public abstract void HasEngine();
+
+            public abstract void HasFourWheels();
+
+            public virtual void HasDiffLock()
+            {
+                Console.WriteLine("Only 4wd's have diff locks");
+            }
+        }
+
+
+        public class FourWD : Automobile
+        {
+            public override void HasDiffLock() { }
+            public override void HasEngine()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void HasFourWheels()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+
+        public class Sedan : Automobile
+        {
+            public override void HasEngine()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void HasFourWheels()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
